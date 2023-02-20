6a2b65a9325f23cd06b418752039c25cede5ae5b
wangbin <wangbin@beyondcent.com>
2023-02-20 15:39:09 +0800

    1、reverseList

--- src/main/java/LC/day01/TwoSum.java
+++ src/main/java/LC/day01/TwoSum.java
@@ -51,7 +51,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
